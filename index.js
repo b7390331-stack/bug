c27e2f02547f42eaa3364d349f1d789dceef2b9c
b7390331-stack <b7390331@gmail.com>
2025-12-05 14:00:07 +0500

    add project

---
+++ index.js
@@ -0,0 +1,151 @@
+// var ism="Bekzodjon"
+// console.log(ism)
+
+// alert("ismingiz nima ")
+
+// var ism = prompt("ismingiz nima ?")
+
+// alert("Sizning ismingiz :" + ism)
+
+// console.log(Number("123"));    
+// console.log(Number("3.14"));   
+// console.log(Number("0"));     
+// console.log(Number("5a"));     
+// console.log(Number("hello")); 
+// console.log(parseInt("123"));      
+// console.log(parseInt("3.14"));   
+// console.log(parseInt("50px"));    
+// console.log(parseInt("hello123")); 
+// console.log(parseInt("9.99abc")); 
+
+// console.log("Number:", Number("3.14"));    
+// console.log("parseInt:", parseInt("3.14"));
+
+
+// console.log("Number:", Number("50px"));     
+// console.log("parseInt:", parseInt("50px")); 
+
+
+// console.log("Number:", Number("10"));       
+// console.log("parseInt:", parseInt("10"));
+
+
+//======== uyga vazifa =============== Uy ishi =======
+
+
+// let a = 5;
+// console.log(a++ + ++a);
+// let x = 10;
+// console.log(++x + x++ + ++x);
+// let k = 0;
+// console.log(++k + ++k + k++ + k);
+// let s = 5;
+// let q = ++s - s++ + --s + s++;
+
+
+// let yosh = 16;
+
+// if (yosh >= 18) {
+//     console.log("Kirish mumkin");
+// } else {
+//     console.log("Ruxsat yo'q");
+// }
+
+// let number = -5;
+
+// if (number > 0) {
+//     console.log("Musbat son");
+// } else if (number < 0) {
+//     console.log("Manfiy son");
+// } else {
+//     console.log("0 ga teng");
+// }
+
+// let n = 7;
+
+// if (n % 2 === 0) {
+//     console.log("Juft son");
+// } else {
+//     console.log("Toq son");
+// }
+
+// let a = 12;
+// let b = 5;
+// let c = 9;
+
+// let engKatta;
+
+// if (a >= b && a >= c) {
+//     engKatta = a;
+// } else if (b >= a && b >= c) {
+//     engKatta = b;
+// } 
+// else {
+//     engKatta = c;
+// }
+
+// console.log("Eng katta son:", engKatta);
+
+// let ball = 87;
+
+// if (ball >= 0 && ball <= 59) {
+//     console.log("Qoniqarsiz");
+// } else if (ball >= 60 && ball <= 74) {
+//     console.log("Qoniqarli");
+// } else if (ball >= 75 && ball <= 89) {
+//     console.log("Yaxshi");
+// } else if (ball >= 90 && ball <= 100) {
+//     console.log("A'lo");
+// } else {
+//     console.log("Noto‘g‘ri ball kiritildi");
+// }
+
+
+// let temp = 32;
+
+// if (temp < 0) {
+//     console.log("Juda sovuq");
+// } else if (temp >= 0 && temp <= 20) {
+//     console.log("Sovuq");
+// } else if (temp >= 21 && temp <= 35) {
+//     console.log("Yoqimli ob-havo");
+// } else if (temp > 35) {
+//     console.log("Juda issiq");
+// } else {
+//     console.log("Noto‘g‘ri qiymat");
+// }
+
+// let password = "abc123";
+
+// if (password.length < 8) {
+//     console.log("Parol juda qisqa");
+// } else {
+//     console.log("Parol qabul qilindi");
+// }
+
+// let text = "";
+
+// if (text === "") {
+//     console.log("Matn kiritilmadi");
+// } else {
+//     console.log("Matn qabul qilindi");
+// }
+
+
+// sinf ishi
+
+
+// let son = prompt("sonni kiriting :")
+
+// if (son > 0) {
+//     alert("Musbat son")
+// }else if (son < 0){
+//     alert("Manfiy son")
+// }else ("0 musbat ham manfiy ham emas")
+
+let i = 0;
+// true
+while(true){
+
+    console.log(i++)
+}
